d28163f2187979f2e327a157fe37d3c0f47328e4
Ivan Penev <ivan.penev92@gmail.com>
2018-02-01 11:08:35 +0100

    DNI Initial

---
+++ DNI/src/main/java/org/mvpigs/dni/ComprobacionDNI.java
@@ -0,0 +1,7 @@
+package org.mvpigs.dni;
+
+public class ComprobacionDNI {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ DNI/src/main/java/org/mvpigs/dni/check.java
@@ -0,0 +1,8 @@
+package org.mvpigs.dni;
+
+public class check {
+    private String DNI;
+    private char letter;
+    private char[] letters = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q',
+                            'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z'};
+}
