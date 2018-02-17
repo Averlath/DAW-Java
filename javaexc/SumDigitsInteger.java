81b39289f16962ca283ec6cb2d33b6a113f00bf3
Ivan Penev <ivan.penev92@gmail.com>
2018-02-17 19:11:47 +0100

    Ejercicios Basicos Java

--- javaexc/SumDigitsInteger.java
+++ javaexc/SumDigitsInteger.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class SumDigitsInteger {
     public static void main(String[] args) {
         Scanner text = new Scanner(System.in);
-        System.out.print("Input an integer: ");
+        System.out.print("Input a number: ");
         int num = text.nextInt();
         System.out.println("The sum of the digits is: " + sumDigits(num));
     }
