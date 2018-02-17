a5a048c950488d1b5c54aecbf0d2bd072208b452
Ivan Penev <ivan.penev92@gmail.com>
2018-02-17 19:54:03 +0100

    Cambios clase BlockChain

--- ExamenFebrero_Intento#2/src/main/java/org/mvpigs/Pigcoin/BlockChain.java
+++ ExamenFebrero_Intento#2/src/main/java/org/mvpigs/Pigcoin/BlockChain.java
@@ -1,33 +1,30 @@
 package org.mvpigs.Pigcoin;
 
-import org.mvpigs.Pigcoin.Transaction;
-import org.mvpigs.Pigcoin.Wallet;
-import org.mvpigs.Pigcoin.GenSig;
-
-import java.security.PublicKey;
+import java.util.ArrayList;
 
 public class BlockChain {
-    private String[] blockChain = null; // lo componen las transacciones de pigcoins que han realizado los propietarios de las wallets
+    private ArrayList<Transaction> blockChain = new ArrayList<Transaction>(); // lo componen las transacciones de pigcoins que han realizado los propietarios de las wallets
+
+    // Constructor
+    public BlockChain() {
+    }
+
+    //Metodos
+    public ArrayList<Transaction> getBlockChain() {
+        return this.blockChain;
+    }
 
     public void summarize() {
-        System.out.println(blockChain);
+        for (Transaction transaction : blockChain) {
+            System.out.println(transaction.toString());
+        }
+    }
+
+    public void summarize(int posicion) {
+        System.out.println(blockChain.get(posicion));
     }
 
     public void addOrigin(Transaction transaction) {
         System.out.println(transaction);
     }
-
-    public void processTransactions(PublicKey pKey_sender, PublicKey pKey_recipient, int consumedCoins, String message, byte[] signedTransaction) {}
-
-    public void isConsumedCoinValid(int consumedCoins) {}
-
-    public void isSignatureValid(PublicKey pKey_sender, String message, byte[] signedTransaction) {}
-
-    public void createTransaction(PublicKey pKey_sender, PublicKey pKey_recipient, int consumedCoins, String message, byte[] signedTransaction) {}
-
-    public void loadInputTransactions(PublicKey address) {}
-
-    public void loadOutputTransactions(PublicKey address) {}
-
-    public void loadWallet(PublicKey address) {}
 }
