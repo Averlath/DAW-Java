f3f3dc348f0c36188d6c5c68327823071de33e47
Ivan Penev <ivan.penev92@gmail.com>
2018-04-07 14:12:07 +0200

    Primer

---
+++ Romans/src/main/java/org/mvpigs/Romans/Romans.java
@@ -0,0 +1,11 @@
+package org.mvpigs.Romans;
+
+public class Romans {
+    public static void main(String[] args) {
+        String test = "MMMDCCCLXXXVIII"; // 3888
+		test = "MMDCCLXXVII";  // 2777
+		test = "CDXLIV"; // 444
+        test = "CDXXXIX"; // 439
+        
+    }
+}
