35a30701f814dd18d986e1ce03ffa7785e3a7412
Ivan Penev <ivan.penev92@gmail.com>
2018-04-07 14:22:54 +0200

    set/get numero

---
+++ Romans/src/main/java/org/mvpigs/Romans/Numero.java
@@ -0,0 +1,25 @@
+package org.mvpigs.Romans;
+
+public class Numero {
+    private String numeroRomano = null;
+    private int numeroDecimal = 0;
+
+    public NumeroRomano() {
+    }
+
+    public void setNumeroRomano(String numeroRomano) {
+        this.numeroRomano = numeroRomano;
+    }
+
+    public String getNumeroRomano() {
+        return this.numeroRomano;
+    }
+
+    public void setNumeroDecimal(int numeroDecimal) {
+        this.numeroDecimal = numeroDecimal;
+    }
+
+    public int getNumeroDecimal() {
+        return this.numeroDecimal;
+    }
+}
